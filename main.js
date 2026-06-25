d9eaa1c74bbcf18a027eeb626fa1a30c8553bacd
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-06-25 20:04:35 +0500

    funksiya yozildi

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ let rasmlar = [
     "https://picsum.photos/id/600/400?random=2",
     "https://picsum.photos/id/600/400?random=3",
 ]
+
+let i = 0;
+
+const rasmElement = document.getElementById("rasm");
+
+function next(){
+    i = (i + 1) % rasmlar.length;
+    rasmElement.src = rasmlar[i];
+}
+
+function prev(){
+    i = (i - 1 + rasmlar.length) % rasmlar.length;
+    rasmElement.src = rasmlar[i];
+}
